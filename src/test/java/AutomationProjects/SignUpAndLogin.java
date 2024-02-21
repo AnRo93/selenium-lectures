c74f5348d4a9438f504a125d13a0d9cba47cb426
Andy Ro <andyroro93@gmail.com>
2024-02-20 19:13:40 -0800

    Automation Project

---
+++ src/test/java/AutomationProjects/SignUpAndLogin.java
@@ -0,0 +1,92 @@
+package AutomationProjects;
+
+import com.github.javafaker.Faker;
+import org.checkerframework.checker.units.qual.K;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+import java.awt.dnd.DragGestureEvent;
+import java.time.Duration;
+
+public class SignUpAndLogin {
+
+
+    @Test
+    public void test() throws InterruptedException{
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("http://duotify.us-east-2.elasticbeanstalk.com/register.php");
+
+        Assert.assertTrue(driver.getTitle().contains("Welcome to Duotify!"));
+
+        driver.findElement(By.id("hideLogin")).click();
+
+
+        Faker faker = new Faker();
+        String user = faker.address().firstName();
+        String first = faker.address().firstName();
+        String last = faker.address().lastName();
+        String email = faker.internet().emailAddress();
+        String pass = faker.internet().password();
+        driver.findElement(By.name("username")).sendKeys(
+                user, Keys.TAB,
+                first, Keys.TAB,
+                last, Keys.TAB,
+                email, Keys.TAB,
+                email, Keys.TAB,
+                pass, Keys.TAB, pass, Keys.ENTER);
+
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://duotify.us-east-2.elasticbeanstalk.com/browse.php?");
+
+        String firstAndLastName = driver.findElement(By.xpath("//span[@id='nameFirstAndLast']"))
+                .getText();
+        Assert.assertEquals(firstAndLastName, first + " " + last);
+
+        driver.findElement(By.xpath("//span[@id='nameFirstAndLast']"))
+                .click();
+
+        driver.findElement(By.xpath("//div[@class='userInfo']"))
+                .getText();
+
+        Assert.assertEquals(firstAndLastName, first + " " + last);
+
+        driver.findElement(By.xpath("//button[@id='rafael']"))
+                .click();
+
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://duotify.us-east-2.elasticbeanstalk.com/register.php");
+
+        driver.findElement(By.name("loginUsername")).sendKeys(
+                user, Keys.TAB,
+                pass, Keys.TAB, Keys.ENTER);
+
+        driver.findElement(By.xpath("//h1[@class='pageHeadingBig']"))
+                .getText();
+
+        Assert.assertTrue(driver.getPageSource().contains("You Might Also Like"));
+
+        driver.findElement(By.xpath("//span[@id='nameFirstAndLast']"))
+                .click();
+
+        driver.findElement(By.xpath("//button[@id='rafael']"))
+                .click();
+
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "http://duotify.us-east-2.elasticbeanstalk.com/register.php");
+
+    }
+
+
+
+}
